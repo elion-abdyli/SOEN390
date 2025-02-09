df94790b14c9590be8c01c112e87020a78405ba5
Mustafa-M422 <158519759+Mustafa-M422@users.noreply.github.com>
2025-02-08 21:13:41 -0500

    Update SwitchButton.test.js

--- components/_tests_/SwitchButton.test.js
+++ components/_tests_/SwitchButton.test.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
-import SwitchButtons from '../Map/SwitchButton'; // Make sure the import path is correct
+import SwitchButtons from '../MapComponents/SwitchBotton';
 
 describe('SwitchButtons Component', () => {
   it('renders correctly with both buttons', () => {
-    const { getByText } = render(<SwitchButtons onSwitchToSGW={() => {}} onSwitchToLoyola={() => {}} />);
-
+    const { getByText } = render(
+      <SwitchButtons onSwitchToSGW={() => {}} onSwitchToLoyola={() => {}} />
+    );
     expect(getByText('Switch to SGW')).toBeTruthy();
     expect(getByText('Switch to Loyola')).toBeTruthy();
   });
 
   it('calls onSwitchToSGW when "Switch to SGW" is pressed', () => {
     const mockOnSwitchToSGW = jest.fn();
-    const { getByText } = render(<SwitchButtons onSwitchToSGW={mockOnSwitchToSGW} onSwitchToLoyola={() => {}} />);
-
+    const { getByText } = render(
+      <SwitchButtons onSwitchToSGW={mockOnSwitchToSGW} onSwitchToLoyola={() => {}} />
+    );
     fireEvent.press(getByText('Switch to SGW'));
     expect(mockOnSwitchToSGW).toHaveBeenCalledTimes(1);
   });
 
   it('calls onSwitchToLoyola when "Switch to Loyola" is pressed', () => {
     const mockOnSwitchToLoyola = jest.fn();
-    const { getByText } = render(<SwitchButtons onSwitchToSGW={() => {}} onSwitchToLoyola={mockOnSwitchToLoyola} />);
-
+    const { getByText } = render(
+      <SwitchButtons onSwitchToSGW={() => {}} onSwitchToLoyola={mockOnSwitchToLoyola} />
+    );
     fireEvent.press(getByText('Switch to Loyola'));
     expect(mockOnSwitchToLoyola).toHaveBeenCalledTimes(1);
   });
 });
-
-
