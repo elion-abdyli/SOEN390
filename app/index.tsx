bbc27344b23922e966c2b442509a831fe70c57c1
Sarah <41saraho@gmail.com>
2025-02-16 11:51:21 -0500

    Added Smartlook

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,14 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import Navigation from '../navigation/Navigation';
 
+import { SMARTLOOK_API_KEY } from '@/constants/SmartlookKey';
+import Smartlook from 'react-native-smartlook-analytics'
+
+Smartlook.instance.preferences.setProjectKey(
+  SMARTLOOK_API_KEY 
+);
+Smartlook.instance.start();
+
 export default function App() {
   return (
     <>
