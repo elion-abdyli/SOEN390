e3feca7cf2566da67012facabd4a272cb742a73c
Elion Abdyli <53652464+elion-abdyli@users.noreply.github.com>
2025-03-03 03:55:24 -0500

    fix duration service

--- services/DurationService.ts
+++ services/DurationService.ts
@@ -1,9 +1,8 @@
-import { GOOGLE_MAPS_API_KEY } from "@/constants/GoogleKey";
 import { LatLng } from "react-native-maps";
 
-export const getTripDuration = async (origin: LatLng, destination: LatLng): Promise<number> => {
+export const getTripDuration = async (origin: LatLng, destination: LatLng, apiKey: string): Promise<number> => {
     // api call to get only the duration of a trip with distance matrix, ensure it is enabled in your google api
-    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin.latitude},${origin.longitude}&destinations=${destination.latitude},${destination.longitude}&mode=driving&key=${GOOGLE_MAPS_API_KEY}`;
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin.latitude},${origin.longitude}&destinations=${destination.latitude},${destination.longitude}&mode=driving&key=${apiKey}`;
 
     const response = await fetch(url);
     const data = await response.json();  // get the data from the API call
