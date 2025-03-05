f792c380bc0dc0b82ca14f78433ebf41ecba1d4a
Elion Abdyli <53652464+elion-abdyli@users.noreply.github.com>
2025-03-04 22:47:15 -0500

    add go button

--- screens/MapExplorerScreen.tsx
+++ screens/MapExplorerScreen.tsx
@@ -153,6 +153,10 @@ export default function MapExplorerScreen() {
     }
   };
 
+  const handleGoPress = () => {
+    console.log("GO button pressed");
+  };
+
   return (
     <View style={DefaultMapStyle.container}>
       {/* Our map & markers */}
@@ -183,6 +187,9 @@ export default function MapExplorerScreen() {
           </Button>
           <Button mode="contained" onPress={handleCenterToUserLocation} style={ButtonsStyles.button}>
             ME
+          </Button>
+          <Button mode="contained" onPress={handleGoPress} style={ButtonsStyles.button}>
+            GO
           </Button>
         </View>
       </View>
