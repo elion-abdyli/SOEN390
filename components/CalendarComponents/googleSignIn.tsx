27f877510905fee2d7489adde17f54ca2d98c527
Ziad-Tari <tarikziad347@gmail.com>
2025-04-03 22:44:18 -0400

    Making the base for google sign in

---
+++ components/CalendarComponents/googleSignIn.tsx
@@ -0,0 +1,12 @@
+import { WEB_CLIENT_ID } from '@/constants/GoogleKey';
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+  } from '@react-native-google-signin/google-signin';
+  import { View, Alert, ScrollView, Dimensions, Text, Modal, TouchableOpacity, StatusBar } from "react-native";
+import { signIn } from './signIn';
+
+
+
+
