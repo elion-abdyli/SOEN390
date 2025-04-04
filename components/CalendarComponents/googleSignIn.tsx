ca092e2336011b722152a781fb9d345473687dc5
Ziad-Tari <tarikziad347@gmail.com>
2025-04-03 22:44:42 -0400

    Applying coniguration for the sign in

--- components/CalendarComponents/googleSignIn.tsx
+++ components/CalendarComponents/googleSignIn.tsx
@@ -10,3 +10,25 @@ import { signIn } from './signIn';
 
 
 
+  GoogleSignin.configure({
+    webClientId: WEB_CLIENT_ID,
+    scopes: ['https://www.googleapis.com/auth/drive.readonly',
+            'https://www.googleapis.com/auth/calendar.readonly'],
+    offlineAccess: true, 
+    forceCodeForRefreshToken: false, 
+  });
+
+
+  export default function GoogleSign () {
+return (
+
+<View>
+    <Text>I AM BACKKKKK</Text>
+    <GoogleSigninButton
+    size={GoogleSigninButton.Size.Wide}
+    color={GoogleSigninButton.Color.Dark}
+    onPress={signIn}/>
+    <StatusBar></StatusBar>
+</View>
+);
+  }
