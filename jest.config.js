209b479dce9921cd2ed8146d2c7b60243759c156
adssib <75389300+adssib@users.noreply.github.com>
2025-04-05 19:05:21 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,6 @@ module.exports = {
     preset: 'jest-expo',
     testEnvironment: 'node',
     collectCoverage: true,
-    coverageThreshold: {
-      global: {
-        statements: 80,
-        branches: 70,
-        functions: 80,
-        lines: 80,
-      },
-    },
     collectCoverageFrom: [
       'components/InputComponents/**/*.{js,jsx,ts,tsx}',
       'components/MapComponents/**/*.{js,jsx,ts,tsx}',
