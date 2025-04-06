29c8e213c5ff6e92a07d460cff1fdaea3b9968e0
Omar Elmasaoudi <omarelmasaoudi04@gmail.com>
2025-04-06 07:29:03 -0400

    Changes made to resolve merge conflicts

--- components/CalendarComponents/googleSignIn.tsx
+++ components/CalendarComponents/googleSignIn.tsx
@@ -1,13 +1,7 @@
 import { WEB_CLIENT_ID } from '@/constants/GoogleKey';
-import {
-    GoogleSignin,
-    GoogleSigninButton,
-    statusCodes,
-  } from '@react-native-google-signin/google-signin';
+import { GoogleSignin, GoogleSigninButton, statusCodes} from '@react-native-google-signin/google-signin';
   import { View, Alert, ScrollView, Dimensions, Text, Modal, TouchableOpacity, StatusBar } from "react-native";
 import { signIn } from './signIn';
-
-
 
 
   GoogleSignin.configure({
